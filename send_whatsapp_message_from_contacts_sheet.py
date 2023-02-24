723d41ceae4a6ef6fdec7959e61ef0174b128f3b
arpansahu <arpanrocks95@gmail.com>
2023-02-25 00:25:46 +0530

    Initial commit

---
+++ send_whatsapp_message_from_contacts_sheet.py
@@ -0,0 +1,261 @@
+import pandas as pd
+import time
+import pywhatkit
+import pyautogui
+from pynput.keyboard import Key, Controller
+
+keyboard = Controller()
+
+
+def send_whatsapp_message(msg, mob_no):
+    try:
+        pywhatkit.sendwhatmsg_instantly(
+            phone_no=f"+{mob_no}",
+            message=msg,
+            tab_close=True
+        )
+        time.sleep(10)
+        pyautogui.click()
+        time.sleep(2)
+        keyboard.press(Key.enter)
+        keyboard.release(Key.enter)
+        print(f"Message sent! to {mob_no}")
+    except Exception as e:
+        print(str(e))
+
+
+if __name__ == "__main__":
+    # Load the xlsx file
+
+    for file in ['HOPELESS.xlsx', 'NAMELESS.xlsx', 'SHAMELESS.xlsx']:
+        excel_data = pd.read_excel('MCH V4 Level 2 Agent.xlsx')
+        breakpoint()
+        for mob_no in excel_data['WhatsApp Number(with country code)']:
+            print(mob_no)
+
+            send_whatsapp_message(
+                msg="JOIN MY TELEGRAM CHANNEL FOR FURTHER UPDATES"
+                , mob_no= mob_no
+            )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
