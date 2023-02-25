66990fdd180ac2f4d794502d66010b684cf05390
arpansahu <arpanrocks95@gmail.com>
2023-02-26 01:09:07 +0530

    3rd

--- google_contacts_adder.py
+++ google_contacts_adder.py
@@ -17,8 +17,8 @@ for file in os.listdir('/Users/arpansahu/projects/whastapp_automator/Contacts Sh
             contact_name = saved_name_arr[count]
             mob_no = mob_no_arr[count]
 
-            if '+' in contact_name:
-                final_name_array.append(f'MCH MEMBER {member_count}')
+            if 'EARNFREEONLINE' not in contact_name and 'MCH' not in contact_name:
+                final_name_array.append(f'GSK MEMBER {member_count}')
                 final_mob_no_array.append(mob_no)
                 member_count += 1
         except Exception as e:
